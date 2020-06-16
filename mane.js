76a03f9a66f637f9310e51e4123825e27e6170e3
hedonist666 <43249611+hedonist666@users.noreply.github.com>
2020-06-16 21:50:42 +0300

    Update mane.js

--- mane.js
+++ mane.js
@@ -3,6 +3,7 @@
 
 const http = require('http');
 http.createServer(function (req, res) {
+    console.log('got request')
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('it is running\n');
 }).listen(process.env.PORT || 5000);
